0a99e551fdb8f4562efdce52acdaf871e9e243d2
cry-inc <manuel@caputo.de>
2012-11-22 18:49:11 +0100

    Added HTML generation to Formular

--- src/de/htwg/mgse/formular/model/Input.java
+++ src/de/htwg/mgse/formular/model/Input.java
@@ -24,5 +24,10 @@ public class Input extends Element {
 	public void setDefaultValue(String defaultValue) {
 		this.defaultValue = defaultValue;
 	}
+	
+	public String toHtml() {
+		return	"\t" + label + ":<br />\n" + 
+				"\t<input name=\"" + id + "\" value=\"" + defaultValue + "\" type=\"" + type.toString() + "\" /><br />\n";
+	}
 
 }
